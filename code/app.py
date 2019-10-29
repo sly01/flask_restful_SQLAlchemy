6d8e6853abf4e2b77447357375c9bb3b9d9cb6b8
sly01 <aerkoc01@gmail.com>
2019-10-29 10:41:29 +0100

    Added SQLAlchemy for ORM

--- code/app.py
+++ code/app.py
@@ -7,6 +7,7 @@ from resources.user import UserRegister
 from resources.item import Item, ItemList
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'aerkoc'
 api = Api(app)
 
@@ -17,4 +18,7 @@ api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 
-app.run(port=5000, host="0.0.0.0", debug=True)
+if __name__ == '__main__':
+    from db import db
+    db.init_app(app)
+    app.run(port=5000, host="0.0.0.0", debug=True)
