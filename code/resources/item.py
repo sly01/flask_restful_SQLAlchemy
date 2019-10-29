3d1d7e1f5f519ddee9531d9f8375e9b107ffc25b
sly01 <aerkoc01@gmail.com>
2019-10-29 11:19:45 +0100

    User model and resouce adjusted

--- code/resources/item.py
+++ code/resources/item.py
@@ -64,13 +64,6 @@ class Item(Resource):
 
 class ItemList(Resource):
     def get(self):
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "SELECT * from items"
-        result = cursor.execute(query)
-        items = []
-        for row in result:
-            items.append({'name': row[0], 'price': row[1]})
-        connection.close()
-        return {'items': items}
+        return {'items': [item.json for item in ItemModel.query.all()]}
+        # Pythonic way ! 
+        # return {'items': list(map(lambda x: x.json(), ItemModel.query.all()))}
